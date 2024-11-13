f9489f4149f95dccac0f662c2091e0ad9210867e
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-11-13 19:22:18 +0300

    fix/some bugs in code

--- dist/index.js
+++ dist/index.js
@@ -10,8 +10,13 @@ var itemsRouter_1 = require("./routes/itemsRouter");
 var carsRouter_1 = require("./routes/carsRouter");
 var PORT = 5000;
 var app = (0, express_1.default)();
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, //access-control-allow-credentials:true
+    // optionSuccessStatus:200
+};
 var parserMiddleware = (0, body_parser_1.default)({});
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)(corsOptions));
 app.use(parserMiddleware);
 app.use('/product', itemsRouter_1.itemsRouter);
 app.use('/cars', carsRouter_1.carsRouter);
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,15 @@ import {carsRouter} from "./routes/carsRouter";
 
 const PORT =  5000;
 const app = express()
-
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,            //access-control-allow-credentials:true
+    // optionSuccessStatus:200
+}
 
 
 const parserMiddleware = bodyParser({})
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(parserMiddleware)
 app.use('/product',itemsRouter)
 app.use('/cars',carsRouter)
